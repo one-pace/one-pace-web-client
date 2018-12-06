4da0dc259f7485dce66d43c7a9e28fa9bbc1cb0c
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-12-06 23:23:29 +0100

    ffghfc

--- react/js/components/Layout.jsx
+++ react/js/components/Layout.jsx
@@ -8,6 +8,7 @@ export default class Layout extends React.Component {
 					<a href="/"><img className="logo" src="/assets/Logo.png" /></a>
 					<a className="link" href="/about">About</a>
 					<a className="link" href="/donate">Donate</a>
+					<a className="discord link" target="_blank" rel="noopener noreferrer" href="https://discordapp.com/invite/WHpTJdP"><img src="/assets/discord.svg" /></a>
 				</div>
 				<div className="layout-container">
 					<div className="main-content">
