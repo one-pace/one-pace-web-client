fecf67998f5ed5c96d6a31d6cebb4c5f944a6f18
Simon Haga <simon.haga.users@gmail.com>
2018-12-07 14:38:34 +0100

    backgroundSize to css

--- react/js/components/ArcSideBox.jsx
+++ react/js/components/ArcSideBox.jsx
@@ -11,7 +11,7 @@ export default class ArcSideBox extends React.Component {
 			<div className={"arc-side-box" + (this.props.isSelected ? " selected" : "")}>
 				<div className="arc-header">
 					<div onClick={this.props.onClick} className="arc-header-container">
-						<div className="arc-img" style={{"backgroundSize": "cover", "backgroundImage": "url(/assets/arc_" + arc.id + ".png)"}} />
+						<div className="arc-img" style={{"backgroundImage": "url(/assets/arc_" + arc.id + ".png)"}} />
 						<div className="arc-info">
 							<div className="arc-title">{title}</div>
 							<div className="arc-subtitle">{subtitle}</div>
