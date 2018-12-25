0029ba78aee2fa7370f0af784877787ecd9da2ce
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-12-25 15:47:11 +0100

    Fix unreleased styling

--- react/js/components/Overview/Card.jsx
+++ react/js/components/Overview/Card.jsx
@@ -13,10 +13,10 @@ export default class Card extends React.Component {
 		<div className="progress-card" onClick={this.props.onClick}>
 			<img className="list-image" src={this.props.img} />
 		</div> ||
-		<div className={"progress-card title" + (episode.in_progress ? " unreleased" : "")} onClick={this.props.onClick}>
+		<div className={"progress-card title" + (episode.in_progress ? " unreleased" : "")} onClick={!episode.in_progress && this.props.onClick}>
 			<div className="text">{episode.part ? arc.title + " " + episode.part.toString().padStart(2, "0") : episode.title}</div>
 			{ episode.title && episode.part && <div className="status">“{episode.title}”</div> }
-			<div className="status">{episode.released_date && Moment(episode.released_date, "YYYY-MM-DD HH:mm:ss").format("MMMM D, YYYY") || "TBA"}</div>
+			{!episode.in_progress&&<div className="status">{episode.released_date && Moment(episode.released_date, "YYYY-MM-DD HH:mm:ss").format("MMMM D, YYYY") || "In progress"}</div>}
 			{
 				episode.chapters && episode.episodes && <div className="status">Ch. {episode.chapters} / Ep. {episode.episodes}</div>
 				|| episode.chapters && <div className="status">Chapter {episode.chapters}</div>
