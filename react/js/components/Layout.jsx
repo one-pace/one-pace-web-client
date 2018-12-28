2027cd712b9d72cacb7b1d7601651df7afa960ae
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-12-28 23:41:33 +0100

    Remove excel sheet link

--- react/js/components/Layout.jsx
+++ react/js/components/Layout.jsx
@@ -11,7 +11,6 @@ export default class Layout extends React.Component {
 					<a className="link text-link" href="/overview">Overview</a>
 					<a className="link link-right" target="_blank" rel="noopener noreferrer" href="https://discordapp.com/invite/WHpTJdP"><img src="/assets/discord.svg" /></a>
 					<a className="link link-right" target="_blank" rel="noopener noreferrer" href="http://forums.arlongpark.net/showthread.php?t=38681"><img src="/assets/arlongpark.svg" /></a>
-					<a className="link link-right" target="_blank" rel="noopener noreferrer" href="https://onedrive.live.com/view.aspx?resid=2A9137DBF1112637!37021&amp;ithint=file%2cxlsx&amp;app=Excel&amp;authkey=!ACgbEWtkXaBEMC0"><img src="/assets/excel.svg" /></a>
 				</div>
 				<div className="main-content">
 					{this.props.children}
