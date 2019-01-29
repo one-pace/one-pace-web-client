b9c061d64c122b21e0d9b6fdfc42dca7760a1ea4
Simon Haga <1498414+sewil@users.noreply.github.com>
2019-01-29 16:43:43 +0100

    add github link, remove qc recruit announcement

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,10 +8,10 @@ export default class Layout extends React.Component {
 					<a href="/"><img className="logo" src={require("../images/Logo.png")} /></a>
 					<a className="link text-link" href="/about">About</a>
 					<a className="link text-link" href="/overview">Overview</a>
-					<a className="link link-right" target="_blank" rel="noopener noreferrer" href="https://paypal.me/supportonepace"><img src={require("../images/paypal.svg")} /></a>
-					<a className="link link-right" target="_blank" rel="noopener noreferrer" href="https://discordapp.com/invite/WHpTJdP"><img src={require("../images/discord.svg")} /></a>
-					<a className="link link-right" target="_blank" rel="noopener noreferrer" href="http://forums.arlongpark.net/showthread.php?t=38681"><img src={require("../images/arlongpark.svg")} /></a>
-					<span className="link link-right text-link">Currently looking for QCers!</span>
+					<a className="link link-right" title="GitHub" target="_blank" rel="noopener noreferrer" href="https://github.com/one-pace/onepace.net"><img src={require("../images/github.svg")} /></a>
+					<a className="link link-right" title="Paypal" target="_blank" rel="noopener noreferrer" href="https://paypal.me/supportonepace"><img src={require("../images/paypal.svg")} /></a>
+					<a className="link link-right" title="Discord" target="_blank" rel="noopener noreferrer" href="https://discordapp.com/invite/WHpTJdP"><img src={require("../images/discord.svg")} /></a>
+					<a className="link link-right" title="Arlong Park Forums" target="_blank" rel="noopener noreferrer" href="http://forums.arlongpark.net/showthread.php?t=38681"><img src={require("../images/arlongpark.svg")} /></a>
 				</div>
 				<div className="main-content">
 					{this.props.children}
