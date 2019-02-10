5130f47333179253524ef5cca6a11c7eba47b2ce
Simon Haga <simon.haga94@gmail.com>
2019-02-10 07:21:43 +0100

    gettorrent json

--- php/get_torrent.php
+++ php/get_torrent.php
@@ -3,20 +3,36 @@ require_once 'RequestHeader.php';
 require_once 'db_context.php';
 include_once 'torrent_utils.php';
 $torrents = TorrentUtils::getTorrents();
-$context = new db_context();
-$context->connect();
-$episode = $context->read_episode($_GET['episode']);
-$context->disconnect();
-
-if($episode == null) {
-	echo "Episode not found";
-} else {
-	$torrent = TorrentUtils::findTorrent($torrents, $episode["torrent_hash"]);
-	if($torrent == null) {
-		echo "Torrent with hash '" . $episode['torrent_hash'] . "' not found.";
-	} else if (isset($_GET['magnet']) && $_GET['magnet'] == "true" && $torrent['magnet'] != null) {
-		header("Location: " . $torrent['magnet']);
+if(isset($_GET['episode'])) {
+	$context = new db_context();
+	$context->connect();
+	$episode = $context->read_episode($_GET['episode']);
+	$context->disconnect();
+	if($episode == null) {
+		echo "Episode not found";
+		exit;
 	} else {
-		header("Location: https://onepace.net/torrents/" . $torrent["torrent_name"]);
+		$torrent_hash = $episode["torrent_hash"];
 	}
+} else if(isset($_GET['torrent_hash'])) {
+	$torrent_hash = $_GET['torrent_hash'];
+}
+
+if($torrent_hash == null || strlen($torrent_hash) == 0) {
+	echo "No torrent hash found.";
+	exit;
+}
+
+$torrent = TorrentUtils::findTorrent($torrents, $torrent_hash);
+if($torrent == null) {
+	echo "Torrent with hash '" . $torrent_hash . "' not found.";
+	exit;
+}
+
+if(isset($_GET['json']) && $_GET['json'] == "true") {
+	echo json_encode($torrent);
+} else if (isset($_GET['magnet']) && $_GET['magnet'] == "true" && $torrent['magnet'] != null) {
+	header("Location: " . $torrent['magnet']);
+} else {
+	header("Location: https://onepace.net/torrents/" . $torrent["torrent_name"]);
 }
