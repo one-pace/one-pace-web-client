c4caef9f6fdc982dd273453cfb4c93ec570a8e09
Simon Haga <1498414+sewil@users.noreply.github.com>
2019-07-07 23:45:36 +0200

    Tabs

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -3,62 +3,62 @@ var webpack = require("webpack")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-	"entry": "./src/components/Index.jsx",
-	"output": {
-		"path": path.join(__dirname, "../dist"),
-		"filename": "index.js"
-	},
-	"plugins": [
-		new webpack.ProvidePlugin({
-			"$": "jquery",
-			"jQuery": "jquery",
-			"jquery": "jquery"
-		}),
-		new webpack.DefinePlugin({
-			"process.env.NODE_ENV": JSON.stringify("development")
-		}),
-		new CopyWebpackPlugin([{ from: "../server", to: "server" }], {})
-	],
-	"resolve": {
-		"extensions": [".js", ".jsx"]
-	},
-	"module": {
-		"rules": [
-			{
-				"test": /\.s?css$/,
-				"use": [
-					"style-loader",
-					"css-loader",
-					"sass-loader"
-				]
-			},
-			{
-				"test": /\.jsx?$/,
-				"loader": "babel-loader",
-				"include": [path.join(__dirname, "/src"), path.join(__dirname, "node_modules/reflux-core")],
-				"options": {
-					"presets": ["react", "es2015", "stage-1", "stage-2"]
-				}
-			},
-			{
-				"test": /\.(woff2?|png|jpe?g|ico|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-				"loader": "file-loader",
-				"options": {
-					"name": "[name].[ext]",
-					"outputPath": "assets/"
-				}
-			},
-			{
-				"test": /\.html$/,
-				"loader": "file-loader",
-				"options": {
-					"name": "[name].[ext]"
-				}
-			}
-		]
-	},
-	"watch": true,
-	"watchOptions": {
-		"ignored": /node_modules/
-	}
+  "entry": "./src/components/Index.jsx",
+  "output": {
+    "path": path.join(__dirname, "../dist"),
+    "filename": "index.js"
+  },
+  "plugins": [
+    new webpack.ProvidePlugin({
+      "$": "jquery",
+      "jQuery": "jquery",
+      "jquery": "jquery"
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("development")
+    }),
+    new CopyWebpackPlugin([{ from: "../server", to: "server" }], {})
+  ],
+  "resolve": {
+    "extensions": [".js", ".jsx"]
+  },
+  "module": {
+    "rules": [
+      {
+        "test": /\.s?css$/,
+        "use": [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        "test": /\.jsx?$/,
+        "loader": "babel-loader",
+        "include": [path.join(__dirname, "/src"), path.join(__dirname, "node_modules/reflux-core")],
+        "options": {
+          "presets": ["react", "es2015", "stage-1", "stage-2"]
+        }
+      },
+      {
+        "test": /\.(woff2?|png|jpe?g|ico|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        "loader": "file-loader",
+        "options": {
+          "name": "[name].[ext]",
+          "outputPath": "assets/"
+        }
+      },
+      {
+        "test": /\.html$/,
+        "loader": "file-loader",
+        "options": {
+          "name": "[name].[ext]"
+        }
+      }
+    ]
+  },
+  "watch": true,
+  "watchOptions": {
+    "ignored": /node_modules/
+  }
 }
