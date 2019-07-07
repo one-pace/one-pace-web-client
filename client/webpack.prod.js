8ac82bb5b2de488cb3b869a59fc20959389c5792
Simon Haga <1498414+sewil@users.noreply.github.com>
2019-07-07 23:47:19 +0200

    Quotes

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,60 +1,60 @@
-﻿var path = require("path")
-var webpack = require("webpack")
-const CopyWebpackPlugin = require("copy-webpack-plugin")
+﻿var path = require('path')
+var webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  "entry": "./src/components/Index.jsx",
-  "output": {
-    "path": path.join(__dirname, "../dist"),
-    "filename": "index.js"
+  'entry': './src/components/Index.jsx',
+  'output': {
+    'path': path.join(__dirname, '../dist'),
+    'filename': 'index.js'
   },
-  "plugins": [
+  'plugins': [
     new webpack.ProvidePlugin({
-      "$": "jquery",
-      "jQuery": "jquery",
-      "jquery": "jquery"
+      '$': 'jquery',
+      'jQuery': 'jquery',
+      'jquery': 'jquery'
     }),
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    new CopyWebpackPlugin([{ from: "../server", to: "server" }], {})
+    new CopyWebpackPlugin([{ from: '../server', to: 'server' }], {})
   ],
-  "resolve": {
-    "extensions": [".js", ".jsx"],
-    "alias": {
-      "root": path.join(__dirname, ""),
-      "appRoot": path.join(__dirname, "/src")
+  'resolve': {
+    'extensions': ['.js', '.jsx'],
+    'alias': {
+      'root': path.join(__dirname, ''),
+      'appRoot': path.join(__dirname, '/src')
     }
   },
-  "module": {
-    "rules": [
+  'module': {
+    'rules': [
       {
-        "test": /\.s?css$/,
-        "use": [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
+        'test': /\.s?css$/,
+        'use': [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
       },
       {
-        "test": /\.jsx?$/,
-        "loader": "babel-loader",
-        "include": [path.join(__dirname, "/src"), path.join(__dirname, "node_modules/reflux-core")],
-        "options": {
-          "presets": ["react", "es2015", "stage-1", "stage-2"]
+        'test': /\.jsx?$/,
+        'loader': 'babel-loader',
+        'include': [path.join(__dirname, '/src'), path.join(__dirname, 'node_modules/reflux-core')],
+        'options': {
+          'presets': ['react', 'es2015', 'stage-1', 'stage-2']
         }
       },
       {
-        "test": /\.(woff2?|png|jpe?g|ico|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        "loader": "file-loader",
-        "options": {
-          "name": "[name].[ext]",
-          "outputPath": "assets/"
+        'test': /\.(woff2?|png|jpe?g|ico|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        'loader': 'file-loader',
+        'options': {
+          'name': '[name].[ext]',
+          'outputPath': 'assets/'
         }
       },
       {
-        "test": /\.html$/,
-        "loader": "file-loader?name=[name].[ext]"
+        'test': /\.html$/,
+        'loader': 'file-loader?name=[name].[ext]'
       }
     ]
   }
