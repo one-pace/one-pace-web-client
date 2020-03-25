b75b4645ddb3104c12675ef3be26acd96493bb03
Jakob H Laverdiere <devjakob@gmail.com>
2020-03-25 22:45:56 +0530

    Integrated header links with React Router, and made it a StaticRouter

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { StaticRouter as Router, Route, Switch } from 'react-router-dom'
 import 'babel-polyfill'
 import Watch from './watch'
 import About from './about'
@@ -12,14 +12,14 @@ import '../images/favicon.ico'
 import 'typeface-roboto'
 require.context('../images', false, /arc_\d+\.png$/)
 
-ReactDOM.render(<Router>
-  <Route
-    render={() => (
-      <Layout>
+ReactDOM.render((
+  <Router>
+    <Layout>
+      <Switch>
         <Route exact path='/' component={Watch} />
         <Route name='about' path='/about' component={About} />
         <Route name='overview' path='/overview' component={Overview} />
-      </Layout>
-    )}
-  />
-</Router>, document.getElementById('reactentry'))
+      </Switch>
+    </Layout>
+  </Router>
+), document.getElementById('reactentry'))
