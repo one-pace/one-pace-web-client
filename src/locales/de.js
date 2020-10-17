d6d0141753b8213e7dccadd5b153055bdef8933c
Simon Haga <1498414+sewil@users.noreply.github.com>
2020-10-17 17:44:23 +0200

    Setup i18n and locales

---
+++ src/locales/de.js
@@ -0,0 +1,40 @@
+export default {
+  about: {
+    title: 'What is One Pace?',
+    description: 'One Pace is a team effort that started in March 2013 with the goal of matching the One Piece manga more accurately than Toei\'s anime adaptation. We cut out filler scenes, non-canon reaction shots, padded sequences, and re-order scenes to stay truer to Goda\'s manga.',
+    team: 'The Team',
+    editing: 'Editing',
+    timing: 'Timing',
+    qc: 'QC',
+    qualityChecking: 'Quality checking',
+    vfx: 'Visual effects design',
+    graphics: 'Graphic maker',
+    musicer: 'Music master',
+    webmaster: 'Webmaster',
+    translation: 'Translation',
+    memorialTitle: 'Memorial',
+    memorialDescription: 'In memory of our beloved team member, Feeso, a good man, and our nakama, who stood up for us through thick and thin. After questioning from the F.B.I. proved unsuccessful, he was handed over to CP-9 and locked up in the lowest level of Impel Down. He has not been heard from since.',
+    memorialF: 'F'
+  },
+  overview: {
+    chapter: 'Chapter',
+    chapters: 'Chapters',
+    episode: 'Episode',
+    episodes: 'Episodes',
+    chapterShort: 'Ch.',
+    episodeShort: 'Ep.',
+    tba: 'TBA',
+    unreleased: 'Unreleased'
+  },
+  torrents: {
+    name: 'Name',
+    link: 'Link',
+    size: 'Size',
+    date: 'Date'
+  },
+  nav: {
+    about: 'About',
+    overview: 'Overview',
+    language: 'Language'
+  }
+}
---
+++ src/locales/es.js
@@ -0,0 +1,40 @@
+export default {
+  about: {
+    title: '¿Qué es One Pace?',
+    description: 'One Pace es un proyecto que empezó en marzo de 2013 con el objetivo de igualar el manga de One Piece a una adaptación más fiel de la que ofrece Toei. Eliminamos el relleno, reacciones no-canónicas, escenas alargadas, y re-ordenamos escenas para mantenernos fieles al manga de Goda..',
+    team: 'El Equipo',
+    editing: 'Edición',
+    timing: 'Tiempos',
+    qc: 'QC',
+    qualityChecking: 'Control de calidad',
+    vfx: 'Edición de efectos visuales',
+    graphics: 'Gráficos',
+    musicer: 'Edición de audio',
+    webmaster: 'Diseño web',
+    translation: 'Traducción',
+    memorialTitle: 'Recordatorio',
+    memorialDescription: 'En memoria de nuestro querido miembro del equipo, Feeso, un buen hombre, y nuestro nakama, que se mantuvo firme por nosotros contra viento y marea. Después de que el interrogamiento por parte del F.B.I. no diera resultados, fue entregado al CP-9 y encerrado en el nivel más profundo de Impel Down. Desde entonces, no hemos volvido a escuchar de él.',
+    memorialF: 'F'
+  },
+  overview: {
+    chapter: 'Capítulo',
+    chapters: 'Capítulos',
+    episode: 'Episodio',
+    episodes: 'Episodios',
+    chapterShort: 'Cap.',
+    episodeShort: 'Ep.',
+    tba: 'TBA',
+    unreleased: 'Inédito'
+  },
+  torrents: {
+    name: 'Nombre',
+    link: 'Link',
+    size: 'Tamaño',
+    date: 'Fecha'
+  },
+  nav: {
+    about: 'Acerca de...',
+    overview: 'Sinopsis',
+    language: 'Idioma'
+  }
+}
