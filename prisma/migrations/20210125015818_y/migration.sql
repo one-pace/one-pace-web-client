033d0c99cb946e55f3158a2953761afe2000f8e0
Jakob Laverdiere <jlaverdiere@lodus.io>
2021-01-24 20:59:39 -0500

    Drop `streams_hash` from database

---
+++ prisma/migrations/20210125015818_y/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `streams_hash` on the `Episode` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Episode` DROP COLUMN `streams_hash`;
