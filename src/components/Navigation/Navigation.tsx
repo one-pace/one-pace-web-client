85f1677da21a3eefd0cf0f6951c39ea34d11c8df
Jakob Laverdiere <jlaverdiere@lodus.io>
2021-07-15 17:19:40 -0400

    Started adding header links (WIP)

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -16,14 +16,14 @@ const Navigation: React.FunctionComponent<Props> = ({ className, path }) => {
   return (
     <nav className={cn(s.root, className)}>
       <ul>
-        {/*
-        <li className={cn(s.link, path === '/my-account' && s.active)}>
-          <Link href="/my-account">My Account</Link>
+
+        <li className={cn(s.link, path === '/en/about' && s.active)}>
+          <Link href="/en/about">About</Link>
         </li>
         <li className={cn(s.link, path === '/register' && s.active)}>
-          <Link href="/register">Sign up</Link>
+          <Link href="/login">Sign in</Link>
         </li>
-        */}
+
       </ul>
     </nav>
   );
